8a78513ac3b6f3820b7fe8327ae60d0e966140c8
Shubham Arora <aroras@bu.edu>
2020-08-06 17:15:41 -0400

    show filename in dup2

--- dup2.go
+++ dup2.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 
-	counts := make(map[string]int)
+	counts := make(map[string]map[string]int)
     files := os.Args[1:]
     if len(files) == 0 { // read from std input
         countLines(os.Stdin, counts)
@@ -23,21 +23,37 @@ func main() {
                 fmt.Fprintf(os.Stderr, "Error in opening file, %v\n", err)
                 continue
             }
-            countLines(f, counts);
+            input := bufio.NewScanner(f)
+            for input.Scan() {
+                if counts[input.Text()] == nil {
+                   counts[input.Text()] = make(map[string]int)
+                }
+                counts[input.Text()][arg]++
+            }
             f.Close()
         }
     }
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d, %s\n", n, line)
+
+    for line, occurFiles := range counts {
+        count := 0
+        var keys []string
+        for filename, n := range occurFiles {
+            count+=n
+            keys = append(keys, filename)
+        }
+		if count > 1 {
+			fmt.Printf("%d, %s, files: %v \n", count, line, keys)
 		}
 	}
 }
 
 // map is passed by reference
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]map[string]int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		counts[input.Text()]++
+        if counts[input.Text()] == nil {
+           counts[input.Text()] = make(map[string]int)
+        }
+        counts[input.Text()]["stdin"]++
 	}
 }
